768c8e9961f8d4eb669abc877ae9b90dfa1de83a
Alymansour94 <Alymansour3@yahoo.com>
2021-11-04 19:54:39 +0200

    tgre43

---
+++ hbd.js
@@ -0,0 +1,31 @@
+var day = document.getElementById('day') ;
+var month = document.getElementById('month') ;
+var year = document.getElementById('year') ;
+var btn = document.getElementById('button') ;
+var hbd = document.getElementById('hbd') ;
+var hbde = document.getElementById('hbde') ;
+var firstdiv = document.getElementById('firstdiv') ;
+
+function cong() {
+    
+    firstdiv.style.display='none'
+    hbd.style.display='none'
+
+    if(day.value == "7"  && month.value == "dec" && year.value=="1999") {
+        
+       
+        hbde.style.display='block'
+    }
+
+    else if (day.value == "day"  || month.value == "month" || year.value=="year") {
+        alert('enter your birthday')
+        firstdiv.style.display='block'
+
+    }
+
+    else {
+        hbd.textContent=`happy birthday  `
+        hbd.style.display='block'
+
+    }  
+}
